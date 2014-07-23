8de5136cc1fadbc867293e5991148c0435bccfa5
marcoschwartz <pi@raspberrypi.(none)>
2014-07-23 06:40:21 +0000

    small fix

--- chapter7/interface/public/js/interface.js
+++ chapter7/interface/public/js/interface.js
@@ -1,24 +1,24 @@
 // Hardware parameters
-wifi_address = 'arduino.local';
+wifi_address = '192.168.1.104';
 
-xbee_address = '/dev/cu.usbserial-A702LF8B';
+xbee_address = '/dev/ttyUSB0';
 
 setInterval(function() {
-  
+    
   json_data = send('serial', xbee_address, '/temperature');
   if (json_data.temperature){
      $("#xbeeTempDisplay").html("Temperature: " + json_data.temperature + "°C");    
   }
 
-  json_data = send('serial', xbee_address, '/humidity');
-  if (json_data.humidity){
-    $("#xbeeHumDisplay").html("Humidity: " + json_data.humidity + "%");    
-  }
+  //json_data = send('serial', xbee_address, '/humidity');
+  //if (json_data.humidity){
+    //$("#xbeeHumDisplay").html("Humidity: " + json_data.humidity + "%");    
+  //}
 
-  json_data = send('serial', xbee_address, '/light');
-  if (json_data.light){
-    $("#xbeeLightDisplay").html("Light: " + json_data.light + "%");    
-  }
+  //json_data = send('serial', xbee_address, '/light');
+  //if (json_data.light){
+    //$("#xbeeLightDisplay").html("Light: " + json_data.light + "%");    
+  //}
 
   if (json_data.connected == 1){
     $("#xbeeStatus").html("Online");
@@ -35,11 +35,11 @@ setInterval(function() {
 function buttonClick(clicked_id){
 
   if (clicked_id == "1"){
-    send('wifi', wifi_address, "/digital/8/1");  
+    send('wifi', wifi_address, "/digital/7/1");  
   } 
 
   if (clicked_id == "2"){
-    send('wifi', wifi_address, "/digital/8/0");  
+    send('wifi', wifi_address, "/digital/7/0");  
   }
 
 }
